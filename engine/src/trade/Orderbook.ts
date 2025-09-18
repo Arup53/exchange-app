a3fc737de6562059ec24bc7f71f36dfc574b081b
Arup53 <arupkumarhowlader12@gmail.com>
2025-09-18 09:44:04 +0600

    implement Orderbook class constructor in engine

---
+++ engine/src/trade/Orderbook.ts
@@ -0,0 +1,44 @@
+export interface Order{
+    price:number ;
+    quantity: number;
+    orderId: string ;
+    filled : number;
+    side:"buy" | "sell";
+    userId: string ;
+}
+
+export interface Fill{
+    price: string ;
+    qty: number ;
+    tradeId: number ;
+    otherUserId: string ;
+    markerOrderId: string ;
+}
+
+export const BASE_CURRENCY="USD";
+
+
+export class OrderBook{
+    bids: Order[];
+    asks: Order[];
+    baseAsset: string;
+    quoteAsset: string= BASE_CURRENCY ;
+    lastTradeId: number;
+    currentPrice:number ;
+
+    constructor(baseAsset:string, bids: Order[], asks: Order[], lastTradeId: number, currentPrice: number){
+        this.bids= bids;
+        this.asks= asks ;
+        this.baseAsset= baseAsset;
+        this.lastTradeId = lastTradeId || 0 ;
+        this.currentPrice = currentPrice || 0;
+    }
+
+    
+
+
+
+
+
+
+}
