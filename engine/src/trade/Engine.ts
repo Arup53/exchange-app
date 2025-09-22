9380f3599733905b928cf83ec856dedef18531f5
Arup53 <arupkumarhowlader12@gmail.com>
2025-09-22 10:58:32 +0600

    implement checkAndLockFunds method in engine in engine package

--- engine/src/trade/Engine.ts
+++ engine/src/trade/Engine.ts
@@ -1,4 +1,5 @@
-import { OrderBook } from "./Orderbook";
+import { CREATE_ORDER, MessageFromApi } from "../types/fromApi";
+import { Fill, Order, OrderBook } from "./Orderbook";
 
 export const BASE_CURRENCY="USD";
 
@@ -15,7 +16,31 @@ export class Engine {
     private balances: Map<string, UserBalance> = new Map();
 
 
-    process({}):{message:, clientId:string}{
-        
+    checkAndLockFunds(baseAsset: string, quoteAsset: string, side:"buy"|"sell", userId: string, asset: string, price: string, quantity: string){
+        if(side==="buy"){
+            if((this.balances.get(userId)?.[quoteAsset]?.avaliable || 0) < Number(quantity)*Number(price)){
+                throw new Error("Insufficient funds");
+            }
+
+            // @ts-ignore
+            this.balances.get(userId)[quoteAsset].avaliable = this.balances.get(userId)?.[quoteAsset].avaliable - (Number(quantity)*Number(price));
+             //@ts-ignore
+            this.balances.get(userId)[quoteAsset].locked = this.balances.get(userId)?.[quoteAsset].locked + (Number(quantity) * Number(price));
+        } else {
+            if ((this.balances.get(userId)?.[baseAsset]?.avaliable || 0)< Number(quantity)){
+                throw new Error("Insufficient funds");
+            }
+
+            // @ts-ignore
+            this.balances.get(userId)[baseAsset].avaliable= this.balances.get(userId)?.[baseAsset].avaliable -(Number(quantity));
+
+            // @ts-ignore
+            this.balances.get(userId)[baseAsset].locked = this.balances.get(userId)?.[baseAsset].locked + Number(quantity);
+        }
     }
+
+   
+
+
+
 }
