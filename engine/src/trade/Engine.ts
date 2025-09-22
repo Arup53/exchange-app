20751cc5bd6a3feccb177f6d3f388ccedf8e093a
Arup53 <arupkumarhowlader12@gmail.com>
2025-09-22 09:35:58 +0600

    create fromApi type in engine package

--- engine/src/trade/Engine.ts
+++ engine/src/trade/Engine.ts
@@ -1,4 +1,21 @@
-
-
+import { OrderBook } from "./Orderbook";
 
 export const BASE_CURRENCY="USD";
+
+interface UserBalance {
+    [key:string]:{
+        avaliable: number ;
+        locked: number ;
+    }
+}
+
+
+export class Engine {
+    private orderbooks: OrderBook[]=[];
+    private balances: Map<string, UserBalance> = new Map();
+
+
+    process({}):{message:, clientId:string}{
+        
+    }
+}
