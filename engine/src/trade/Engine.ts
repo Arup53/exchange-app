226209d8329ec749e3740bc2ca2d9afd3b0da644
Arup53 <arupkumarhowlader12@gmail.com>
2025-09-22 11:31:17 +0600

    implement constructor  in engine in engine package

--- engine/src/trade/Engine.ts
+++ engine/src/trade/Engine.ts
@@ -1,3 +1,4 @@
+import fs from "fs";
 import { CREATE_ORDER, MessageFromApi } from "../types/fromApi";
 import { Fill, Order, OrderBook } from "./Orderbook";
 
@@ -15,6 +16,36 @@ export class Engine {
     private orderbooks: OrderBook[]=[];
     private balances: Map<string, UserBalance> = new Map();
 
+    constructor() {
+        let snapshot = null
+        try {
+            if (process.env.WITH_SNAPSHOT) {
+                snapshot = fs.readFileSync("./snapshot.json");
+            }
+        } catch (e) {
+            console.log("No snapshot found");
+        }
+
+        if (snapshot) {
+            const snapshotSnapshot = JSON.parse(snapshot.toString());
+            this.orderbooks = snapshotSnapshot.orderbooks.map((o: any) => new OrderBook(o.baseAsset, o.bids, o.asks, o.lastTradeId, o.currentPrice));
+            this.balances = new Map(snapshotSnapshot.balances);
+        } else {
+            this.orderbooks = [new OrderBook(`TATA`, [], [], 0, 0)];
+            this.setBaseBalances();
+        }
+        setInterval(() => {
+            this.saveSnapshot();
+        }, 1000 * 3);
+    }
+
+    saveSnapshot() {
+        const snapshotSnapshot = {
+            orderbooks: this.orderbooks.map(o => o.getSnapshot()),
+            balances: Array.from(this.balances.entries())
+        }
+        fs.writeFileSync("./snapshot.json", JSON.stringify(snapshotSnapshot));
+    }
 
 
 
